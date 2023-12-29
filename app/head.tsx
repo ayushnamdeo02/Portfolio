a18db7b61ec418bd4ab1124e8a40c7059fd34742
ayushnamdeo02 <ayushnamdeo.an@gmail.com>
2023-12-29 11:34:09 +0530

    commit

---
+++ app/head.tsx
@@ -0,0 +1,23 @@
+export default function Head() {
+  return (
+    <>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="I'm Ayush Namdeo,Artificial Intelligence Aspirent" />
+      <meta name="keywords" content="Ayush Namdeo, portfolio, ayush, full stack dev, nextjs portfolio, portfolio design, portfolio website, personal portfolio" />
+      <meta property="og:site_name" content="Ayush Namdeo's Portfolio" />
+      <meta property="og:title" content="Portfolio | Ayush Namdeo" />
+      <meta property="og:description" content="I'm Ayush Namdeo,Lives in Nagpur, India." />
+      <meta property="og:url" content="https://ayushnamdeo02.vercel.app" />
+      <meta property="og:image" content="https://ayushnamdeo02.vercel.app/portfolio-fork.png" />
+      <link rel="canonical" href="https://ayushnamdeo02.vercel.app" />
+      <meta name="author" content="Ayush Namdeo" />
+      <meta name="language" content="English" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="https://ayushnamdeo02.vercel.app" />
+      <meta name="application-name" content="Portfolio | Portfolio | Ayush Namdeo" />
+      <meta name="apple-mobile-web-app-title" content="Portfolio | Ayush Namdeo" />
+      <link rel="icon" href="/favicon.ico" />
+      <title>Portfolio | Ayush Namdeo</title>
+    </>
+  )
+}
